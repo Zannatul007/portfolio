eed41cfddd14f2dae4ad58da5bad65a6480b576d
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2025-09-06 22:35:09 +0600

    navbar side

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,58 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary navbar-custom">
-      <Container>
-        <Navbar.Brand as={Link} to="/home">
-          Zannatul Fardaush Tripty
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav-gap">
-            <Nav.Link as={Link} to="/home">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about">
-              About
-            </Nav.Link>
-            <Nav.Link as={Link} to="/education">
-              Education
-            </Nav.Link>
-            <Nav.Link as={Link} to="/publication">
-              Publications
-            </Nav.Link>
-            <Nav.Link as={Link} to="/research">
-              Experience
-            </Nav.Link>
-            <Nav.Link as={Link} to="/projects">
-              Projects
-            </Nav.Link>
-            <Nav.Link as={Link} to="/achievement">
-              Achievements & Activities
-            </Nav.Link>
-            <Nav.Link as={Link} to="/training">
-              Training
-            </Nav.Link>
-            <Nav.Link as={Link} to="/skills">
-              Skills
-            </Nav.Link>
-            {/* Optional Contact link */}
-            {/* <Nav.Link as={Link} to="/contact">Contact</Nav.Link> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary navbar-custom">
+        <Container>
+          <Navbar.Brand as={Link} to="/home">
+            Zannatul Fardaush Tripty
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="start" // "start" for left side, "end" for right side
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={Link} to="/home">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/about">
+                  About
+                </Nav.Link>
+                <Nav.Link as={Link} to="/education">
+                  Education
+                </Nav.Link>
+                <Nav.Link as={Link} to="/publication">
+                  Publications
+                </Nav.Link>
+                <Nav.Link as={Link} to="/research">
+                  Experience
+                </Nav.Link>
+                <Nav.Link as={Link} to="/projects">
+                  Projects
+                </Nav.Link>
+                <Nav.Link as={Link} to="/achievement">
+                  Achievements & Activities
+                </Nav.Link>
+                <Nav.Link as={Link} to="/training">
+                  Training
+                </Nav.Link>
+                <Nav.Link as={Link} to="/skills">
+                  Skills
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 };
